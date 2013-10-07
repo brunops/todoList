4925fd6dfb60d955226df7ea5082a39cb0bd68d4
Bruno Sanches <brunopsanches@gmail.com>
2013-10-06 22:11:50 -0700

    Create TodoView

--- js/app.js
+++ js/app.js
@@ -10,3 +10,43 @@ var myTodo = new Todo({
   title: 'My todo item'
 });
 
+var TodoView = Backbone.View.extend({
+  tagName: 'li',
+
+  // Cache the template function for a single item.
+  todoTpl: _.template( $('#item-template').html() ),
+
+  events: {
+    'dbclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
+  },
+
+  // Called when the view is first created
+  initialize: function() {
+    this.$el = $('#todo');
+  },
+
+  // Re-render the titles of the todo item.
+  render: function() {
+    this.$el.html( this.todoTpl( this.model.toJSON() ) );
+    this.input = this.$('.edit');
+    return this;
+  },
+
+  edit: function() {
+    // executed when todo label is double clicked
+  },
+
+  close: function() {
+    // executed when todo loses focus
+  },
+
+  updateOnEnter: function( e ) {
+    // executed on each keypress when in todo edit mode,
+    // but we'll wait for enter to get in action
+  }
+});
+
+var todoView = new TodoView({ model: myTodo });
+
